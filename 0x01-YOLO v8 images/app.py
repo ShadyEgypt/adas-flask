a226e614f517ce31a959454b8115f711ebbb9503
ShadyEgypt <shadyrafat60@gmail.com>
2024-06-13 08:11:25 +0300

    update 0x01-yolov8 project

--- 0x01-YOLO v8 images/app.py
+++ 0x01-YOLO v8 images/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from botocore.exceptions import ClientError
 import boto3
 import segmentModule
 import json
 import os
-from urllib.parse import unquote
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -14,26 +13,6 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/hello', methods=['GET'])
 def hello():
     return 'Hello, world!'
-
-def file_exists(name):
-    try:
-        # Create an S3 client
-        s3 = boto3.client('s3')
-        
-        s3_bucket_name = 'adas-project-bucket'
-        file_key = f'results/{name}'
-        
-        # Try to head the object to check if it exists
-        response = s3.head_object(Bucket=s3_bucket_name, Key=file_key)
-        print(f"File '{file_key}' exists in bucket '{s3_bucket_name}'.")
-        return 0
-    except ClientError as e:
-        if e.response['Error']['Code'] == '404':
-            print(f"File '{file_key}' does not exist in bucket '{s3_bucket_name}'.")
-            return 1
-        else:
-            print(f"Error checking file existence: {e}")
-            return 1
 
 
 def download_from_s3(bucket_name, file_key, local_file_path): 
@@ -49,7 +28,7 @@ def upload_to_s3(file_key, local_file_path):
     s3.upload_file(local_file_path, bucket_name, file_key)
     # os.remove('result.jpg')
 
-@app.route('/seg-img', methods=['POST'])
+@app.route('/seg-yolov8-image', methods=['POST'])
 def segment_image():
     try:
         data = request.json
@@ -63,7 +42,9 @@ def segment_image():
         download_from_s3(image_bucket_name, image_file_key,img_file_path)
         response = segmentModule.segment(img_file_path)
         print(response)
-        upload_to_s3(outputS3Key, local_file_path='./result.jpg');
+        upload_to_s3(outputS3Key, local_file_path='./result.jpg')
+        os.remove(img_file_path)
+        os.remove('./result.jpg')
         output_url = f'https://ddx0brhffx34i.cloudfront.net/{outputS3Key}'
         body = {
             "message": "Image uploaded successfully!",
--- 0x01-YOLO v8 images/segmentModule.py
+++ 0x01-YOLO v8 images/segmentModule.py
@@ -1,22 +1,53 @@
+import os
+os.system("pip install ultralytics")
 from ultralytics import YOLO
-from io import BytesIO
 from PIL import Image
 import cv2
-import os
 import json
 
-model = YOLO(f'./best.pt')
 
+def resize(mask, height, width):
+    # Resize the array to match the height of the target shape (1280, 720)
+    resized_array = cv2.resize(mask, (width, height), interpolation=cv2.INTER_LINEAR)
+    return resized_array
 
-def segment(img_file_path):
+def load_model(model_name):
+    compiled_model = YOLO(f'./{model_name}')
+    H, W = 720, 1280
+    return compiled_model, H, W
+
+def preprocess(frame, H, W):
+    """
+    Preprocess the frame for yolov8 model.
+    """
+    image_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    resized_image = cv2.resize(image_bgr, (W, H))
+    return resized_image
+
+def postprocess(results):
+    """
+    Postprocess the frame for yolov8 model.
+    """
+    for r in results:
+        im_array = r.plot(probs=False,conf=False, boxes=False)  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    return im
+
+def segment(img_file_path, model_name, alpha = 0.3):
     try:
+        compiled_model, H, W = load_model(model_name)
         img = cv2.imread(img_file_path)
-        results = model(img)
+        
+        # Preprocess the frame
+        height, width, _ = img.shape
+        input_image = preprocess(img, H, W)
 
-        for r in results:
-            im_array = r.plot(probs=False,conf=False, boxes=False)  # plot a BGR numpy array of predictions
-            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-            im.save('result.jpg')
+        # Perform inference
+        results = compiled_model(input_image)
+
+        # Post-processing steps here...
+        output_image = postprocess(results)
+        output_image.save(f'./result.png')
 
         body = {
             "message": "Image segmented!",
